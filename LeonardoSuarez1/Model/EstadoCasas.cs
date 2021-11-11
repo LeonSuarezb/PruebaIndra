ad3e3e5f5287983f075ddb7f37689c570369fe2b
LeonSuarezb <31070382+LeonSuarezb@users.noreply.github.com>
2021-11-11 10:13:17 -0500

    Add files via upload

---
+++ LeonardoSuarez1/Model/EstadoCasas.cs
@@ -0,0 +1,11 @@
+﻿namespace LeonardoSuarez1.Model
+{
+    /// <summary>
+    /// Estado inicial de las casas
+    /// </summary>
+    public class EstadoCasas
+    {
+        public int[] LstCasas { get; set; }
+        public int Dias { get; set; }
+    }
+}
